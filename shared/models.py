e0a4087aa853e7113ad7771989437007a2939afc
You Gao <henrygao00@gmail.com>
2024-02-22 15:10:50 -0500

    created report and file models

---
+++ shared/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Report(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PENDING'
+        APPROVED = 'APPROVED'
+        REJECTED = 'REJECTED'
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    submission_date = models.DateTimeField(auto_now_add=True)
+    edit_date = models.DateTimeField(auto_now=True)
+    report_text = models.TextField()
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    class Meta:
+        ordering = ['-submission_date']
+        indexes = [
+            models.Index(fields=['submission_date', 'status']),
+        ]
+    def __str__(self):
+        return f'{self.id}: {self.report_text}'
+    
+class File(models.Model):
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    class Meta:
+        indexes = [
+            models.Index(fields=['report', 'file']),
+        ]
+    def __str__(self):
+        return f'{self.report.id}: {self.file}'
+    
+    
+    
