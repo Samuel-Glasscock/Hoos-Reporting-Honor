5a05b07cd42227a09a40d9f5bda52b0d4e3697f6
You Gao <henrygao00@gmail.com>
2024-02-27 16:29:39 -0500

    changed db back to dj_database

--- whistleblower_project/settings.py
+++ whistleblower_project/settings.py
@@ -108,7 +108,12 @@ WSGI_APPLICATION = 'whistleblower_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
+    ),
+    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
