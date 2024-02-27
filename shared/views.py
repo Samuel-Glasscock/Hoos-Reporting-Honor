cc5a8630c45e9bdb1cfd0863959010c8d122ba7c
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-02-26 20:10:01 -0500

    simplified redirect

--- shared/views.py
+++ shared/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     if request.user.profile.is_admin:
-        return render(request, "shared/site.html")
+        return render(request, "login/admin_home.html")
     elif request.user.is_authenticated:
         return render(request, "shared/common.html")
     return render(request, "404.html")
