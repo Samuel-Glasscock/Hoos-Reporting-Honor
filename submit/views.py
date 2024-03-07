14bdc36e36014af5c09ba93729495ee831d531c6
emilyzyzhou <emilyzyzhou@gmail.com>
2024-03-07 18:46:53 +0100

    set up submission views skeleton

--- submit/views.py
+++ submit/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def StartSubmitView(request):
+    return render(request, "submit/start_submission.html")
+
+
+def BackgroundInfoView(request):
+    return render(request, "submit/background_info.html")
+
+
+def InvolvedStudentsView(request):
+    return render(request, "submit/involved_students.html")
+
+
+'''
+def submit_form(request):
+    need to set up later
+'''
