75081bbcc96534b6249a7ad45153a4a8d08a70a4
emilyzyzhou <emilyzyzhou@gmail.com>
2024-03-07 19:57:01 +0100

    removed excessive url

--- submit/urls.py
+++ submit/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = "submit"
 urlpatterns = [
-    path("report/", views.start_submission, name="start_submission"),
-    path('report/<int:submission_id>/background/', views.background_info, name='background_info'),
-    path('report/<int:submission_id>/involved_students', views.involved_students, name='involved_students'),
-    path('report/<int:submission_id>/submission_complete', views.submission_complete, name='submission_complete')
+    path("", views.start_submission, name="start_submission"),
+    path('<int:submission_id>/background/', views.background_info, name='background_info'),
+    path('<int:submission_id>/involved_students', views.involved_students, name='involved_students'),
+    path('<int:submission_id>/submission_complete', views.submission_complete, name='submission_complete')
 ]
