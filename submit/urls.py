2657dfbf688d5b1d33d8eef71d76c7cc4ee7c5cc
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-14 16:00:47 -0400

    created stub submission page for form and logic to redirect anonymous user directly to page that is stub for form

--- submit/urls.py
+++ submit/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import anonymous_submission
 
 app_name = "submit"
 urlpatterns = [
+    path("anonymous_submission/", anonymous_submission, name="anonymous_submission"),
 ]
--- submit/views.py
+++ submit/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def anonymous_submission(request):
+    if request.method == "POST":
+        log.debug("Anonymous submission POST")
+        print(request.POST)
+        # include logic for form here 
+        return render(request, "anonymous_submission.html", {})
