c762bc0c2db06fc9e480323e547f09c1f7964d9b
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-16 23:32:46 -0400

    tied in anonymous submission to new landing pages

--- submit/views.py
+++ submit/views.py
@@ -50,9 +50,9 @@ def submission_complete(request):
 
     return render(request, 'submit/submission_complete.html', {})
 
-def anonymous_submission(request):
-    if request.method == "POST":
-        print(request.POST)
-        # include logic for form here 
-    else:
-        return render(request, "submit/form.html", {})
+# def anonymous_submission(request):
+#     if request.method == "POST":
+#         print(request.POST)
+#         # include logic for form here 
+#     else:
+#         return render(request, "submit/form.html", {})
