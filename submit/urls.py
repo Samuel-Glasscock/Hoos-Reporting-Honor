ddd9507ec24868d9ee688ed280206869cfa7941a
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-16 23:37:23 -0400

    resolved deployment error from missing function name

--- submit/urls.py
+++ submit/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from .views import anonymous_submission
 
 app_name = "submit"
 urlpatterns = [
     path("", views.start_submission, name="start_submission"),
     path("report", views.report, name="report"),
     path("submission_complete", views.submission_complete, name="submission_complete"),
-    path("anonymous_submission/", anonymous_submission, name="anonymous_submission"),
 ]
