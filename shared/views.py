40972caef7f50ca488714d14cd1a439541613ae7
Henry <djx3rn@virginia.edu>
2024-03-17 09:11:33 -0400

    added a blank report create to test

--- shared/views.py
+++ shared/views.py
@@ -21,7 +21,9 @@ def admin_report_list(request):
 
 def upload_test(request):
     if request.method == 'POST':
+        # save a blank report
+        report = Report.objects.create()
         for file in request.FILES.getlist('file'):
-            File.objects.create(file=file)
+            File.objects.create(report=report,file=file)
         return redirect('shared/404.html')
     return render(request, 'shared/upload_test.html')
