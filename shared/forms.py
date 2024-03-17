1b8988c97cc88ddb8a4ac1a90c0ef8f3c1740a5d
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-03-17 03:11:12 -0400

    fixed reference error

--- shared/forms.py
+++ shared/forms.py
@@ -4,7 +4,7 @@ from .models import Report, File
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ["id", "title", "description", "status"]
+        fields = ["report_text", "status"]
 
 
 class FileForm(forms.Form): # not a ModelForm since uploading multiple files
