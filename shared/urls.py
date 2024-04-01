04847d061a6ffc41ea6edad4a097b347ff0c082b
emilyzyzhou <emilyzyzhou@gmail.com>
2024-04-01 19:02:05 -0400

    added some flair

--- shared/urls.py
+++ shared/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "shared"
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("reports/admin", views.admin_report_list, name="admin_report_list"),
     path("upload_test", views.upload_test, name="upload_test"),
     path("view_files", views.view_files, name="view_files"),
