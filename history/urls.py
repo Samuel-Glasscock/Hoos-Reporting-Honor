f851be175985aa878570427ae7b5d0971e8cc2e9
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-04-16 22:56:09 -0400

    removed all deprecated urls that expect case id in url path

--- history/urls.py
+++ history/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = "history"
 urlpatterns = [
-path("case/<int:id>/", views.case, name="case"),
+# path("case/<int:id>/", views.case, name="case"),
 path("lookup/", views.lookup, name="lookup"),
 path("dashboard/", views.dashboard, name="dashboard"),
-path("dashboard/<int:id>", views.report, name="report"), # possibly delete for security?
+# path("dashboard/<int:id>", views.report, name="report"), # possibly delete for security?
 path("delete/", views.delete, name="delete"),
 path("report-details/", views.report_details, name="report_details"),
 ]
