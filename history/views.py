3f6608ac963a67d9b284e1dba9ef2f138b0d0b98
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-04-26 17:53:44 -0400

    added backports for zoneinfo compatability for python version before 3.9

--- history/views.py
+++ history/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.utils import timezone
-from zoneinfo import ZoneInfo
 from django.http import JsonResponse
 import traceback
 import logging
