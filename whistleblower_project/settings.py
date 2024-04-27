5ac65823d7f5215318652b1db8c63f845056ab8b
johndefranco <qbk3xy@virginia.edu>
2024-04-26 23:25:02 -0400

    tests running locally, should pass on github ci now

--- whistleblower_project/settings.py
+++ whistleblower_project/settings.py
@@ -112,17 +112,26 @@ WSGI_APPLICATION = 'whistleblower_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
-    ),
-    'local': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+BASE_DIR = Path(__file__).resolve().parent.parent
+import sys
+
+# Check if the current command involves running tests
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default='fallback_database_url_if_needed',
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
+    }
 
 
 # Password validation
