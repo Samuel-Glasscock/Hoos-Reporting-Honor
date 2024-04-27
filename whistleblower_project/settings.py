797bfe1732bc2361fd8747521c2284d1725cf989
johndefranco <qbk3xy@virginia.edu>
2024-04-27 00:41:16 -0400

    undid testing stuff again

--- whistleblower_project/settings.py
+++ whistleblower_project/settings.py
@@ -111,24 +111,17 @@ WSGI_APPLICATION = 'whistleblower_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import sys
-
-# Check if the current command involves running tests
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
+    ),
+    'local': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        )
-    }
+}
 
 
 # Password validation
