ada260993bf97c1b1b152286683671f26dbb7515
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-04-27 23:43:37 -0400

    readded timezone midddleware

--- whistleblower_project/settings.py
+++ whistleblower_project/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'history.middleware.TimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'whistleblower_project.urls'
